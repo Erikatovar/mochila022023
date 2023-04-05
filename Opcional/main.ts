9a707ca7530b849746a8207fb6c72a6e348766b8
MauriRodiez <c.maurodriguez.a@gmail.com>
2023-04-04 21:12:17 -0300

    Subiendo entrega 5

---
+++ Opcional/main.ts
@@ -0,0 +1,10 @@
+const message:string = "hello world!";
+console.log(message);
+Tiene una ejecucion de tipo compilado.
+
+Es un lenguaje de programacion de nivel alto. Y se presenta multiples ventajas para el desarrollo web y 
+la mejora de la experiencia de usario en apps y sitios webs.
+
+Se compila en JavaScript. Por lo tanto, TS se puede utilizar en cualquier lugar donde se pueda utilizar 
+JS: tanto en el frontend como en el backend.
+Se utiliza en Angular y Stimulus.
---
+++ Opcional/main2.erl
@@ -0,0 +1,11 @@
+% Your program must have a 'main' function.
+
+main(_) -> io:format("Hello world!").
+Tiene un tipo de ejecucion de interprete.
+
+Es un lenguaje de programacion de alto nivel. Tiene una gran capacidad de concurrencia por su habilidad
+para diseñar sistemas tolerantes a fallos y por el hecho de poder mantener y actualizar los componentes 
+del sistema sin realizar paradas en la ejecución. 
+
+Erlang se puede utilizar con el editor de texto "sublime text".
+Se usa en Simple DirectMedia Layer (SDL)
